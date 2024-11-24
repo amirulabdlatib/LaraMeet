6640978d8538066d38a458dfc0296d795178bf5f
Amirul Abd Latib <amirul.latib@alphv.com>
2024-11-24 16:35:08 +0800

    FE searching

--- resources/views/components/user-list.blade.php
+++ resources/views/components/user-list.blade.php
@@ -1,12 +1,25 @@
 <div id="profile-container" class="flex justify-between flex-1 xl:flex-row 2xl:flex-row lg:flex-row md:flex-row flex-col">
     <div class="flex w-full xl:w-[260px] 2xl:w-[260px] md:w-[260px] bg-[#080D14] max-sm:order-1 justify-center">
-        <div class="flex flex-col my-10 itmes-center">
-            <input id="searchField" class="bg-[#202532] text-white rounded w-[80%] h-[45px] px-4 py-2 mx-auto"
-                type="text" name="search" placeholder="Search User">
+        <div class="flex flex-col my-10 w-full items-center">
+            <form action="" method="GET" class="w-full flex justify-center">
+                <div class="w-[80%] relative flex items-center">
+                    <input id="searchField"
+                        class="bg-[#202532] text-white rounded-full w-full h-[35px] px-4 py-2 pr-12 text-sm"
+                        type="text" name="search" placeholder="Search User">
+                    <button type="submit"
+                        class="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-white transition-colors duration-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                        </svg>
+                    </button>
+                </div>
+            </form>
             <div id="results"
                 class="mx-auto xl:my-6 2xl:my-6 lg:my-6 md:my-6 my-0 w-[80%] xl:h-[550px] 2xl:h-[550px] lg:h-[550px] h-full overflow-y-auto space-y-0">
                 @foreach ($users as $user)
-                    <a href="{{ route('profile',$user->username) }}">
+                    <a href="{{ route('profile', $user->username) }}">
                         <div
                             class="flex items-center hover:bg-[#202532] cursor-pointer transition duration-150 ease-out hover:ease-in rounded h-[50px]">
                             <img src="{{ asset($user->profile_image) }}" class="rounded-full w-[30px] h-[30px] ml-2">
