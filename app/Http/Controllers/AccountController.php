fd0d877b0e22aabe4987f6c03b229ec992dc817a
Amirul Abd Latib <amirul.latib@alphv.com>
2024-11-24 21:51:49 +0800

    Restrict user from acessing other account setting

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AccountController extends Controller
 {
-    public function edit()
+    public function edit($account)
     {
+        if( Auth::user()->id != $account){
+            abort(403);
+        }
+
         return view('account');
     }
 
-    public function update()
+    public function update(Request $request)
     {
 
     }
