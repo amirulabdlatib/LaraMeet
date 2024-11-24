450e7a7274f0de41234dde1aedb9abf5ff9f571d
Amirul Abd Latib <amirul.latib@alphv.com>
2024-11-24 15:20:46 +0800

    Cleanup route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 
-Route::get('/home',[HomeController::class, 'home_page'])->name('home');
-Route::get('/',[AuthController::class, 'login_page'])->name('login.page');
-Route::post('/login',[AuthController::class, 'doLogin'])->name('login');
-Route::get('/register',[AuthController::class, 'register_page'])->name('register.page');
-Route::post('/logout',[AuthController::class, 'doLogout'])->name('logout');
+Route::middleware(['guest'])->group(function () {
+
+    Route::get('/',[AuthController::class, 'login_page'])->name('login.page');
+    Route::post('/login',[AuthController::class, 'doLogin'])->name('login');
+    Route::get('/register',[AuthController::class, 'register_page'])->name('register.page');
+});
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/home',[HomeController::class, 'home_page'])->name('home');
+    Route::post('/logout',[AuthController::class, 'doLogout'])->name('logout');
+});
